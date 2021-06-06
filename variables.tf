3547142f261c12a90cceb3b777c4b80647fe4696
chand <chand.nalubolu@gmail.com>
2021-06-06 16:48:23 +0530

    EC2 module

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "imagename" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "machinecount" {}
+variable "sgid" {}
+variable "vpcname" {}
+variable "public-subnets" {}
+variable "iam_instance_profile" {}
